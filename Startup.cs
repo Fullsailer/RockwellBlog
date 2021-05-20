ac4c1b39eeb20c4ffb837b8c9387aa4ad5af1c52
John Flynn <johnbishopflynn@gmail.com>
2021-05-20 18:40:28 -0400

    Deleted SQL Database, updated new and migrated.

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
