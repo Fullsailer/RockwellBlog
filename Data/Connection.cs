fb6e7152a9082bf0f89e1acb841ab6fb9f79033a
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-09-30 13:29:21 -0400

    Added DataUtility
    Commented out conection.cs and added Data Utility

--- Data/Connection.cs
+++ Data/Connection.cs
@@ -5,33 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
 
-namespace RockwellBlog.Data
-{
-    public class Connection
-    {
-        public static string GetConnectionString(IConfiguration configuration)
-        {
-            //var connectionString = configuration.GetConnectionString("DefaultConnection");
-            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-            return string.IsNullOrEmpty(databaseUrl) ? configuration.GetConnectionString("DefaultConnection") : BuildConnectionString(databaseUrl);
-        }
+//namespace RockwellBlog.Data
+//{
+//    public class Connection
+//    {
+//        public static string GetConnectionString(IConfiguration configuration)
+//        {
+//            var connectionString = configuration.GetConnectionString("DefaultConnection");
+//            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+//            return string.IsNullOrEmpty(databaseUrl) ? configuration.GetConnectionString("DefaultConnection") : BuildConnectionString(databaseUrl);
+//        }
 
-        private static string BuildConnectionString(string databaseUrl) 
-        {
-            var databaseUri = new Uri(databaseUrl);
-            var userInfo = databaseUri.UserInfo.Split(":");
+//        private static string BuildConnectionString(string databaseUrl) 
+//        {
+//            var databaseUri = new Uri(databaseUrl);
+//            var userInfo = databaseUri.UserInfo.Split(":");
 
-            return new NpgsqlConnectionStringBuilder()
-            {
-                Host = databaseUri.Host,
-                Port = databaseUri.Port,
-                Username = userInfo[0],
-                Password = userInfo[1],
-                Database = databaseUri.LocalPath.TrimStart('/'),
-                SslMode = SslMode.Prefer,
-                TrustServerCertificate = true
-            }.ToString();
-        }
+//            return new NpgsqlConnectionStringBuilder()
+//            {
+//                Host = databaseUri.Host,
+//                Port = databaseUri.Port,
+//                Username = userInfo[0],
+//                Password = userInfo[1],
+//                Database = databaseUri.LocalPath.TrimStart('/'),
+//                SslMode = SslMode.Prefer,
+//                TrustServerCertificate = true
+//            }.ToString();
+//        }
 
-    }
-}
+//    }
+//}
